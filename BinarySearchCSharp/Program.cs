31f29651e9df78b7d9d40aca689d8b18e90949ce
Viktorio <tukyto99@gmail.com>
2021-07-21 17:47:44 +0300

    Initial commit

---
+++ BinarySearchCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinarySearchCSharp
+{
+    class Program
+    {
+        public static void Main()
+        {
+			int[] arr = { 2, 4, 6, 8, 10, 20, 40, 80 };
+			int x = 10;
+			int index = BinarySearch(arr, 0, arr.Length - 1, x);
+
+			Console.WriteLine(index == -1 ? "Element is not present." : "Element is present at index " + index);
+		}
+
+		public static int BinarySearch(int[] arr, int left, int right, int x)
+		{
+			if (right >= left)
+			{
+				int middle = (left + right) / 2;
+
+				if (x == arr[middle])
+				{
+					return middle;
+				}
+
+				if (x < arr[middle])
+				{
+					return BinarySearch(arr, left, middle - 1, x);
+				}
+
+				return BinarySearch(arr, middle + 1, right, x);
+			}
+
+			return -1;
+		}
+	}
+}
